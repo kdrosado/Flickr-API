a6b50ed410492ab8190144544e8664f60eee6704
Kerry D. Rosado <kdrosado@gmail.com>
2016-09-08 17:38:51 -0700

    Updated app.js
    updated app.js

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
 //Flickr Photo Feeder App using buttons
 
-$(document).ready(function() {//runs after HTML is displayed
-  
-  $('button').click(function (){
-    
-    $("button").removeClass("selected");//removes highlight when other button is selected
-    $(this).addClass("selected");//highlights selected button
-    
-    //access public photos on Flickr API
-    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+$(document).ready(function() {
+
+
+ $('button').click(function () {
+    // highlight the button
+    $("button").removeClass("selected");
+    $(this).addClass("selected");
+
+    // link to Flickr API
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var animal = $(this).text();//holds value of button selected
-   
-    //parse json data
-    var flickrOptions = {
+    var flickrOptions = {//parse json data
       tags: animal,
       format: "json"
     };
-    
-    //callback function displaying the photos
-    function displayPhotos(data){
-      
-      var photoHTML = '<ul>';//holds list of photos
-      //loop though array of photos
-      $.each(data.items, function(i, photo){
+    function displayPhotos(data) {
+      var photoHTML = '<ul>';//list of photos
+      $.each(data.items,function(i,photo) {//loop through list of photos
         photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';//holds photo link
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';//holds photo image
-      });
-      photoHTML += '</ul>';//closes photo list
-      
-      $('#photos').html(photoHTML);//displays photo to HTML page
-      
-    }    
-    
-    $getJSON(flickrAPI, flickrOptions, displayPhotos)
-    
-  });//end of click 
-  
-});//end of ready 
+        photoHTML += '<a href="' + photo.link + '" class="image">';//holds link to photos
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';//holds photo images
+      }); // end each
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);//display photos on html page
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);//get data from Flickr
+
+  }); // end click
+
+}); // end ready
